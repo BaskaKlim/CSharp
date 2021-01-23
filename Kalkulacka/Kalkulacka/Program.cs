db280da6b5cf62e26be8396a243b6f249f2d4509
BaskaKlim <baska.klimekova@gmail.com>
2021-01-23 16:58:36 +0100

    adding input validation

--- Kalkulacka/Kalkulacka/Program.cs
+++ Kalkulacka/Kalkulacka/Program.cs
@@ -8,9 +8,22 @@ namespace Kalkulacka
         {
             //inputs
             Console.WriteLine("Write first number and press Enter");
-            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number1;
+            //adding validation of input if it is integer and parsign into output 
+            while (!int.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("You did not enter correct input, please try again" +
+                    " and press Enter");
+            }
             Console.WriteLine("Write second number and press Enter");
-            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number2;
+            //adding validation of input if it is integer and parsign into output 
+            while (!int.TryParse(Console.ReadLine(), out number2))
+            {
+                Console.WriteLine("You did not enter correct input, please try again" +
+                    " and press Enter");
+            }
+
             Console.WriteLine("Choose operation:");
             Console.WriteLine("\t+");
             Console.WriteLine("\t-");
