6accd8eb504818df6d812cff5457820bb5218a58
BaskaKlim <baska.klimekova@gmail.com>
2021-01-23 17:10:29 +0100

    init package structure for Fizz-bizz exercise

---
+++ Fizz-buzz/Fizz-buzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fizz_buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
