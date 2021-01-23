2d90e4452f349f6dda5bb286f9ca95160e00f875
BaskaKlim <baska.klimekova@gmail.com>
2021-01-23 16:14:57 +0100

    simple calculator using Console.ReadLine and Console.Writeline, switch cases

--- Kalkulacka/Kalkulacka/Program.cs
+++ Kalkulacka/Kalkulacka/Program.cs
@@ -6,7 +6,30 @@ namespace Kalkulacka
     {
         static void Main(string[] args)
         {
-        
+            //inputs
+            String inputNumber1 = Console.ReadLine();
+            int number1 = Convert.ToInt32(inputNumber1);
+            String inputNumber2 = Console.ReadLine();
+            int number2 = Convert.ToInt32(inputNumber2);
+
+            String operation = Console.ReadLine();
+            //options
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(number1 + number2);
+                    break;
+                case "-":
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case "*":
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case "/":
+                    Console.WriteLine(number1 / number2);
+                    break;
+            }
         }
     }
 }
