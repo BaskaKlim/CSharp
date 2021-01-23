4bc1da072828568189f10232ae5eb2a1ea64e88d
BaskaKlim <baska.klimekova@gmail.com>
2021-01-23 17:17:56 +0100

    simple solution by checking modulo

--- Fizz-buzz/Fizz-buzz/Program.cs
+++ Fizz-buzz/Fizz-buzz/Program.cs
@@ -6,7 +6,26 @@ namespace Fizz_buzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO: Zadanie: Maj číslo. Ak číslo bude deliteľné číslom 3, tak na výpise konzoly
+            // bude text Fizz ak bude deliteľné číslom 5, tak na výpise bude text Buzz.
+            // Ak bude číslo zároveň deliteľné 3 a aj 5, tak vypíš FizzBuzz.
+
+
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if ((number % 5 == 0) && (number % 3 == 0))
+            {
+                Console.WriteLine("FizzBuzz");
+
+            }
+            else if (number % 5 == 0)
+            { 
+                Console.WriteLine("Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
         }
     }
 }
