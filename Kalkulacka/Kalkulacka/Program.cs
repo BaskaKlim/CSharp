7ceb73a6ca7c8b528718aca5fa053708215c9660
BaskaKlim <baska.klimekova@gmail.com>
2021-01-23 16:25:52 +0100

    adding descriptions to each steps in console

--- Kalkulacka/Kalkulacka/Program.cs
+++ Kalkulacka/Kalkulacka/Program.cs
@@ -7,11 +7,20 @@ namespace Kalkulacka
         static void Main(string[] args)
         {
             //inputs
+            Console.WriteLine("Write first number and press Enter");
             int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write second number and press Enter");
             int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose operation:");
+            Console.WriteLine("\t+");
+            Console.WriteLine("\t-");
+            Console.WriteLine("\t*");
+            Console.WriteLine("\t/");
 
             String operation = Console.ReadLine();
             //options
+
+            Console.WriteLine("Result is: ");
 
             switch (operation)
             {
