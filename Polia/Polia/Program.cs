a1547a591490a67d6b7756496068c807a87170b3
BaskaKlim <baska.klimekova@gmail.com>
2021-01-24 17:55:43 +0100

    sum and product of items in array

--- Polia/Polia/Program.cs
+++ Polia/Polia/Program.cs
@@ -6,24 +6,41 @@ namespace Polia
     {
         static void Main(string[] args)
         {
-            /*TODO: Maj dvojrozmerné pole kde vnútorné polia majú dĺžku > 5. 
+            /*TODO1: Maj dvojrozmerné pole kde vnútorné polia majú dĺžku > 5. 
              Na konzolu vypíš prvé dve čísla z každého poľa.
-            -Na konzolu vypíš súčet a násobok všetkých čísel v jednorozmernom poli.
-            -Na konzolu vypíš súčet a násobok všetkých čísel v dvojrozmernom poli.
             */
 
             int[][] pole = new int[2][];
             pole[0] = new int[] { 1, 2, 3, 4, 5, 6 };
             pole[1] = new int[] { 7, 8, 9, 10, 11, 12 };
 
-
+            //prejdi pole po celej jeho dlzke
             for (int i = 0; i < pole.Length; i++)
             {
-               
+                //vypis cisla ktore su na indexe 0 a 1 vnutornych poli  
                 Console.WriteLine($"{pole[i][0]},{ pole[i][1]}");
             }
-                   
- 
+
+            // TODO2: Na konzolu vypíš súčet a násobok všetkých čísel v jednorozmernom poli */
+
+            int[] array = new int[] { 2, 4, 6, 8, 10 };
+
+            int sum = 0;
+            int product = 1;
+         
+
+            foreach (int item in array)
+            {
+                sum += item;
+                product = product * item;
+           
+            }
+
+           
+            Console.WriteLine($"Sucet cisel je: {sum}, sucin cisel je: {product}");
+
+            // TODO3:Na konzolu vypíš súčet a násobok všetkých čísel v dvojrozmernom poli.
+
 
 
 
