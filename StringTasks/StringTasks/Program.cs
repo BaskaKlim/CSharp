c916a1056fa82ce399e1d41affcd89da47340779
BaskaKlim <baska.klimekova@gmail.com>
2021-01-25 17:07:14 +0100

    simple method for UpperCasing

--- StringTasks/StringTasks/Program.cs
+++ StringTasks/StringTasks/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringTasks
 {
     class Program
     {
-        static void Main(string[] args)
+       
+
+        static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*TODO: Napíš metódu, ktorá vstupný string zmení na všetko veľké 
+              písmená a vypíše ho na konzolu. */
+  
+            Console.WriteLine("Zadaj slovo");
+            String inputString = Console.ReadLine();
+            UpperString(inputString);
+
+
+            static void UpperString(String inputString)
+            {
+                String outputString = inputString.ToUpper();
+                Console.WriteLine($"{outputString}");
+            }
+
+
+            /*TODO: Napíš metódu, ktorá porovná dva string parametre a na konzolu 
+             napíše true ak su rovnaké a false ak nie sú rovnaké.  */
+
+            /*TODO: Predošlú metódu uprav tak, aby namiesto výpisu true a false 
+             * vrátila boolean true alebo false. */
+
+
+           
+
+
         }
     }
 }
