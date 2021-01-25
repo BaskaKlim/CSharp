15a034979507eb42ecb6dd5d3ea5c433ee3d880f
BaskaKlim <baska.klimekova@gmail.com>
2021-01-25 19:04:54 +0100

    init Terary operator tasks srtucture

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /*TODO:Napíš program, ktorý na konzolu vypíše či je číslo deliteľné 
+         * dvomi alebo nie za pomoci ternárneho operátoru. Nemôžeš použiť if.*/
+
+
+
+        /*TODO:Napíš program, ktorý na konzolu vypíše či je číslo > ako 10, 
+         * < ako 10 alebo = 10 za pomoci ternárneho operátoru v ternárnom operátore.
+         * Nemôžeš použiť if.*/
+
+
+
+        }
+    }
+}
