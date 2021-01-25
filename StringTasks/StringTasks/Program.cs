90d17acbb4a7c2cd67eeaac9c29df995064666b8
BaskaKlim <baska.klimekova@gmail.com>
2021-01-25 16:25:57 +0100

    init String tasks structure

---
+++ StringTasks/StringTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
